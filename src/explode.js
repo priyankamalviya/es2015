502deba6e633e98c6341272c558085c7f28142e4
Priyanka Malviya <priyanka_malviya@icloud.com>
2017-06-28 23:09:37 -0700

    Create explode.js

---
+++ src/explode.js
@@ -0,0 +1,10 @@
+/*Q. Take a string "hello" as input and print h e l l o as output*/
+
+
+function explode(str){
+  console.log (str.split('').join(' '));
+}
+
+explode('hello');
+
+output: h e l l o
